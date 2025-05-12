1dda7b1115c1a4eb44a4fc97236be1a43842d5ed
kozuka yuko <yuzixiaozhong@gmail.com>
2025-05-12 14:01:25 +0900

    ShopRepCOntorollerTest実装中

--- src/tests/Feature/ShopRepControllerTest.php
+++ src/tests/Feature/ShopRepControllerTest.php
@@ -97,23 +97,75 @@ class ShopRepControllerTest extends TestCase
 
         $response = $this->post(route('shopCreateConfirm'), [
             'name' => 'Test Shop',
-            'img_url' => $file,
-            'area' => 1,
-            'genre' => 1,
-            'description' => 'ぜひいらしてください。',
+            'image' => $file,
+            'area' => $this->area->id,
+            'genre' => $this->genre->id,
+            'description' => 'Test description'
         ]);
 
-        $response->assertRedirect(route('shopCreateConfirm'));
+        $response->assertRedirect(route('showShopCreateConfirm'));
 
-        $this->assertNotNull(Session::get('form_input'));
-
+        $this->assertTrue(Session::has('form_input'));
         $sessionData = session('form_input');
 
         $this->assertEquals('Test Shop', $sessionData['name']);
         $this->assertNotNull($sessionData['img_url']);
-        $this->assertEquals(1, $sessionData['area_id']);
-        $this->assertEquals(1, $sessionData['genre_id']);
-        $this->assertEquals('ぜひいらしてください。', $sessionData['description']);
-        $this->assertStringContainsString('images/', $sessionData['img_url']);
+        $this->assertEquals($this->area->id, $sessionData['area_id']);
+        $this->assertEquals($this->genre->id, $sessionData['genre_id']);
+        $this->assertEquals('Test description', $sessionData['description']);
+    
+        $this->assertFileExists(storage_path('app/' . $sessionData['img_url']));
+    }
+
+    /** @test */
+    public function shopCreateConfirm_handles_missing_image()
+    {
+        $area = Area::factory()->create();
+        $genre = Genre::factory()->create();
+
+        $response = $this->post(route('shopCreateConfirm'), [
+            'name' => 'Test Shop',
+            'area' => $area->id,
+            'genre' => $genre->id,
+            'description' => 'Test description'
+        ]);
+
+        $response->assertRedirect(route('showShopCreateConfirm'));
+
+        $sessionData = session('form_input');
+
+        $this->assertNull($sessionData['img_url']);
+    }
+
+    /** @test */
+    public function showShopCreateConfirm_displays_the_confirm_page_with_session_data()
+    {
+        $area = Area::factory()->create(['name' => 'Tokyo']);
+        $genre = Genre::factory()->create(['name' => 'Sushi']);
+
+        session([
+            'form_input' => [
+                'name' => 'Test Shop',
+                'img_url' => 'public/images/test.jpg',
+                'area_id' => $area->id,
+                'genre_id' => $genre->id,
+                'description' => 'Test description'
+            ]
+        ]);
+
+        $response = $this->get(route('showShopCreateConfirm'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('shop_rep.shop_create_confirm');
+        $response->assertViewHas('data', [
+            'name' => 'Test Shop',
+            'img_url' => 'public/images/test.jpg',
+            'area_id' => $area->id,
+            'genre_id' => $genre->id,
+            'description' => 'Test description'
+        ]);
+
+        $response->assertViewHas('area', $area);
+        $response->assertViewHas('genre', $genre);
     }
 }
